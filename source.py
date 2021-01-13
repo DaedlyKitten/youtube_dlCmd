d8ca461ef4d211e6e5a2df56d35213b9de483206
DaedlyKitten <45991388+DaedlyKitten@users.noreply.github.com>
2021-01-14 01:20:03 +0800

    Update source.py

--- source.py
+++ source.py
@@ -1,16 +1,15 @@
-
 import time
 import win32clipboard
-import youtube_dl
+from youtube_dl import YoutubeDL
 
-
-filepath='./'
-# proxy1='http://127.0.0.1:'
+filepath='D:/'
+format1='best' #'bestaudio'
+recordfilepath='D:/videoDLrecord.txt'
 
 ydl_opts = {
-    'format': 'best',
-    # 'proxy' : proxy1,
-    'outtmpl' : filepath+'%(title)s.%(ext)s',  
+    'format': format1,
+    #'proxy' : proxy1,
+    'outtmpl' : filepath+'%(title)s.%(ext)s', 
     'retries': 5,
     'no_warnings': False,   
     'noplaylist':True,
@@ -18,37 +17,41 @@ ydl_opts = {
     # 'ignoreerrors': True,
 }
 
-# print('proxy:', proxy1)
-# print('path: ',filepath)
-print('youtube_dl wrapper, compiled with Pyinstaller.\n@konporer \n-------------\n\n')
 
+[print(x,': ',y) for x,y in ydl_opts.items()]
+print(recordfilepath)
 
 def download(url):
-    print('\n'+str(time.strftime("%c"))+'\n'+url,file=open(filepath+'record.txt','a'))
+    print('\n'+str(time.strftime("%y%m.%d%H%M"))+'\n'+url,file=open(recordfilepath,'a'))
 
     try:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
+        YoutubeDL(ydl_opts).download([url])
     except Exception as e: print(e)
 
+    print('\n------Done!-------\n')
+    checkit(input('\nAnother ?'))
 
 
 def checkit(x):
-    if 'http' in x and input(f'From this url?\n\n{x}\n------------------\n"n" to input another.').lower()!='n': 
-        download(x)
+    if 'http' in x:
+        a=input(f'\n------------------\nFrom this url?\n\n{x}\n-------------------\nOr input another:')    
+        if a.lower()=='': 
+            download(x)
+        else: checkit(a)
     else: 
-        inputed=input('Input url:')
-        if inputed!='': checkit(inputed)
-
-
+        print('Invalid url!')
+        checkit(input(f'\nInput another url:'))
+# def inputUrl():
+#     inputed=input('Input url:')
+#     if inputed!='': checkit(inputed)
     
 
-win32clipboard.OpenClipboard()
-clip = win32clipboard.GetClipboardData()
-win32clipboard.CloseClipboard()
 
-while True:
-    try: checkit(clip)
-    except Exception as e: print(e)
-
+clip=''
+try: 
+    win32clipboard.OpenClipboard()
+    clip = win32clipboard.GetClipboardData()
+    win32clipboard.CloseClipboard()      
+except Exception as e: print('Failed to read clipboard: ', e)
+checkit(clip)
 
